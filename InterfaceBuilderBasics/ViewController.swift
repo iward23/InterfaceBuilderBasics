058a0dfdd236265915c8f0f15f50fed59d43029a
Imani Ward <idward712@gmail.com>
2022-09-15 14:08:08 -0400

    0

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var changeTitle: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func changeT(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
+
 
 
 }
